52a341e9dd43a051d90b597691b74fc5d622bc26
Thomas Campbell <tomcampbell1993@hotmail.co.uk>
2025-08-04 15:59:33 +0100

    synced days and months a bit

--- tom-calendar/src/assets/components/Calendar/Calendar.jsx
+++ tom-calendar/src/assets/components/Calendar/Calendar.jsx
@@ -8,16 +8,53 @@ import './calendar.css'
 export default function Calendar() {
 
     const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth();
+    const year = 2025;
+    const month = 6;
+    const day = 4;
 
-    const daysInMonth = new Date(year, month+1, 0).getDate();
+    date.setMonth(month-1);
+    date.setFullYear(year);
+    date.setDate(day);
 
-    function findDay(dayNumber) {
-        const thatDay = new Date(year, month, dayNumber).getDay();
-        return thatDay
+    console.log(date);
+
+    function displayMonth(thisMonth){
+        switch (thisMonth){
+            case 1:
+                return "January"
+            case 2:
+                return "February"
+            case 3:
+                return "March"
+            case 4:
+                return "April"
+            case 5:
+                return "May"
+            case 6:
+                return "June"
+            case 7:
+                return "July"
+            case 8:
+                return "August"
+            case 9:
+                return "September"
+            case 10:
+                return "October"
+            case 11:
+                return "November"
+            case 12:
+                return "December"
+        }
     }
 
+
+
+    const daysInMonth = new Date(year, month, 0).getDate();
+
+    function findDay(dayNumber) {
+        const thatDay = new Date(year, month-1, dayNumber).getDay();
+        return thatDay
+    }
 
     function createList() {
         const dayList = [];
@@ -38,6 +75,7 @@ export default function Calendar() {
             <div>
                 <Link to='day'>Day</Link>
             </div>
+            <h2>{displayMonth(month)}</h2>
             <div className="day-wrapper">
                 {dayItems}
             </div>
